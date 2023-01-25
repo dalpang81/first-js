a95ed21f593857144bbf67dd2f19d6d7e852a0a8
dalpang81 <leeoy2000@naver.com>
2023-01-25 14:25:05 +0900

    초기프로젝트 업로드

---
+++ app.js
@@ -0,0 +1,7 @@
+function sayHello(nameOfPerson, age, tall, number) {
+	console.log("Hello my name is " + nameOfPerson + age + tall + number);
+}
+
+sayHello("a", 23, 100, 10);
+sayHello("b");
+sayHello("c");
